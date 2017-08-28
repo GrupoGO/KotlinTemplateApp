3d3808a774b64a7b6d2ae8ab5ce0f4e1744dfa1b
acmeuno <caolmech@gmail.com>
2017-08-28 12:54:16 +0200

    ImageExtensions

---
+++ app/src/main/java/es/grupogo/cocktailsapp/extensions/ImageViewExtensions.kt
@@ -0,0 +1,19 @@
+package es.grupogo.cocktailsapp.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import es.grupogo.cocktailsapp.R
+
+/**
+ * Created by carlosolmedo on 28/8/17.
+ */
+inline fun ImageView.loadImage(url: String) {
+
+    val options = RequestOptions()
+            .centerCrop()
+            .placeholder(R.mipmap.ic_launcher)
+            .error(R.mipmap.ic_launcher)
+
+    Glide.with(this).load(url).apply(options).into(this)
+}
